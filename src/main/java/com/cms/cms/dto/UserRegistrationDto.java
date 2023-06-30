732cf157f6a0208934362a2839f8e081429624b3
phoggy16 <phogatrohit2001@gmail.com>
2023-07-01 01:39:41 +0530

    fix:added validation for password

--- src/main/java/com/cms/cms/dto/UserRegistrationDto.java
+++ src/main/java/com/cms/cms/dto/UserRegistrationDto.java
@@ -18,7 +18,7 @@ public class UserRegistrationDto {
     private String name;
     @NotBlank(message = "username can't be null")
     private String username;
-    @Pattern(regexp = ".{7,}",message = "password length must be more than or equal to 7")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$", message = "password must have a minimum length of 8 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character")
     @NotBlank(message = "password can't be null")
     private String password;
 }
